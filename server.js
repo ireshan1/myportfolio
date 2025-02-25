d14613d3cb4963f277af431f5a7159885645ad60
ireshan1 <ireshanwaiman@gmail.com>
2025-02-25 05:37:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(cors());
 app.use("/", contactRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));  // Serving static files from the 'client/build' folder
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))  // Make sure the path to index.html is correct
   );
 }
 
