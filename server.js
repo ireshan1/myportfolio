1dd00ada4edbe4c111a809cd146f6ce0961d9b52
ireshan1 <ireshanwaiman@gmail.com>
2025-02-25 05:31:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(cors());
 app.use("/", contactRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
   );
 }
 
